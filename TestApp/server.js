610fefab5827dd68961aa746f1e017fb61d4003c
BLVPrasad <prashaad@gmail.com>
2014-05-30 16:32:10 +0530

    Changed to OpenShift IP & Port.

--- TestApp/server.js
+++ TestApp/server.js
@@ -1,9 +1,20 @@
 /**
  * New node file
  */
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 var http = require('http');
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('NodeJS HElloWorld.......... ');
-}).listen(1337, '127.0.0.1');
+  res.end('Testing NodeJS HElloWorld.......... ');
+});
+
+//server.listen(1337, '127.0.0.1');
+
+server.listen(server_port, server_ip_address, function () {
+	  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
+});
+
 console.log('Server running at blvprasad.com');
